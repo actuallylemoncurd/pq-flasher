4885fdd9c87079b85c5ef7e61e5a0441832f370d
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2023-02-06 10:21:54 -0500

    try boosting just the HCA map

--- 02_patcher.py
+++ 02_patcher.py
@@ -31,9 +31,9 @@ patches = {
         #(0x0005D045, Override, b"\x04"),  # 0x38B is the highest observed clamp limit STOCK
 
         # hca_lm_offset map
-        (0x0005E669, b"\x53\x76\x9C", b"\x20\x20\x20"),  # 0x5E150 map
-        (0x0005E671, b"\x53\x76\x9C", b"\x20\x20\x20"),
-        (0x0005E679, b"\x53\x76\x9C", b"\x20\x20\x20"),
+        (0x0005E669, b"\x53\x76\x9C", b"\x8B\xC5\xFF"),  # 0x5E150 map
+        (0x0005E671, b"\x53\x76\x9C", b"\x8B\xC5\xFF"),
+        (0x0005E679, b"\x53\x76\x9C", b"\x8B\xC5\xFF"),
     ]
 }
 
